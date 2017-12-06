97480dcc21fbed4694d7021ff631e4eab46a45f3
Mimi Chang <mimichang94@yahoo.com>
2017-12-06 20:43:48 +0000

    HW done

--- bamazon.js
+++ bamazon.js
@@ -26,7 +26,73 @@ function afterConnection() {
         if(error) throw error;
         // Log all results of the SELECT statement
         console.log(result);
-        connection.end();
+        userBuyItem();
+    });
+}
+
+function userBuyItem() {
+    // query the database for all items
+    connection.query("SELECT * FROM products", function(err, results) {
+        if(err) throw err;
+        // ask user for which they'd like to buy
+        inquirer
+            .prompt([{
+                    name: "product",
+                    type: "list",
+                    choices: function() {
+                        var allItemsArray = [];
+                        for(var i = 0; i < results.length; i++) {
+                            allItemsArray.push(results[i].product_name);
+                        }
+                        return allItemsArray;
+                    },
+                    message: "What would you like to buy? Enter in the number."
+                },
+                {
+                    name: "stock",
+                    type: "input",
+                    message: "How many units would you like to buy?"
+                }
+            ])
+            .then(function(answer) {
+                // get the information of the chosen item
+                //error inside of the .then function
+                console.log(answer);
+                answer.stock = parseInt(answer.stock);
+                var dbItem;
+                for(var i = 0; i < results.length; i++) {
+                    if(results[i].product_name === answer.product) {
+                        dbItem = results[i];
+                    }
+                }
+
+                // determine if stock_quantity is enough
+                console.log(dbItem);
+                if(dbItem.stock_quantity > answer.stock) {
+                    var newStockQuantity = dbItem.stock_quantity - answer.stock;
+                    // if yes
+                    connection.query(
+                        "UPDATE products SET ? WHERE ?", [{
+                                stock_quantity: newStockQuantity
+                            },
+                            {
+                                id: dbItem.id
+                            }
+                        ],
+                        function(error) {
+                            if(error) throw error;
+                            console.log("Congrats! You have bought this item!");
+                            console.log("Total Cost: " + (dbItem.price * answer.stock).toFixed(2));
+                            connection.end();
+                        }
+                    );
+                }
+                else {
+                    // not enough quantity
+                    console.log("We don't have enough to fulfull your order...");
+                    connection.end();
+                }
+            });
     });
 }
 
